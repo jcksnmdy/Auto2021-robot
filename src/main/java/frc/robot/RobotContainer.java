ea254e6de4443fcc08a72158b6edea09511768ac
Jackson Moody <jcksnmdy1@gmail.com>
2021-01-28 14:37:14 -0500

    Init

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,248 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVoltageConstraint;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.controller.RamseteController;
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
+import frc.robot.Constants.AutoConstants;
+
+import frc.robot.subsystems.ComplexDrivetrain;
+import frc.robot.subsystems.Index;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooting;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.DriveLimeLight;
+import frc.robot.commands.AutoCommands;
+
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import java.util.List;
+
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  // Subsystems
+	public static ComplexDrivetrain drivetrain = new ComplexDrivetrain();
+	public static Shooting shooter = new Shooting();
+	public static Index index = new Index();
+	public static Intake intake = new Intake();
+	private DigitalInput m_limitSwitch = new DigitalInput(1);
+	private boolean limelightOn = false;
+
+	private boolean intakeOn = false;
+	// Controllers
+	public static Joystick joystick = new Joystick(0);
+	public static JoystickButton circle = new JoystickButton(joystick, 3);
+	public static JoystickButton leftTrigger = new JoystickButton(joystick, 7);
+	public static JoystickButton rightTrigger = new JoystickButton(joystick, 8);
+	public static JoystickButton xbutton = new JoystickButton(joystick, 2);
+	public static JoystickButton slowTurn = new JoystickButton(joystick, 12);
+	// public static DriveToDistance driveToDistance = new DriveToDistance(10,
+	// drivetrain);
+	public static DriveLimeLight driveLimelight = new DriveLimeLight(drivetrain);
+	public static AutoCommands autoCommands = new AutoCommands(drivetrain);
+
+  private RunCommand m_curvatureDrive = new RunCommand(() -> {
+		if (slowTurn.get()) {
+			drivetrain.curvatureDrive(((joystick.getRawAxis(1)) / 3), ((joystick.getRawAxis(2)) / 3));
+		} else {
+			drivetrain.curvatureDrive(((joystick.getRawAxis(1)) / 1.5), ((joystick.getRawAxis(2) / 2.5)));
+		}
+  }, drivetrain);
+  
+  private RunCommand runIndexer = new RunCommand(() -> {
+		rightTrigger.whenReleased(new InstantCommand(() -> index.stopIndexer(), index));
+		if (circle.get()) {
+			index.reverse();
+		}
+		if (rightTrigger.get()) {
+			index.runIndexer();
+		} else if (m_limitSwitch.get()) {
+			index.stopIndexer();
+		}
+  }, index);
+  
+  private RunCommand runShooter = new RunCommand(() -> {
+		if (leftTrigger.get()) {
+			shooter.shootLimeLight();
+		} else if (xbutton.get()) {
+			shooter.shoot(-0.75);
+		} else {
+			shooter.stopShooting();
+		}
+	}, shooter);
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the button bindings
+    configureButtonBindings();
+    configureDefaultCommands();
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+    new JoystickButton(joystick, 4).whenPressed(new InstantCommand(() -> {
+			if (intakeOn == true) {
+				intake.stopIntake();
+				index.stopIndexer();
+				intakeOn = false;
+			} else {
+				intake.runIntake();
+				// index.runIndexer();
+				intakeOn = true;
+			}
+
+    }, intake, index));
+    
+		// X (on ps4) -
+		new JoystickButton(joystick, 1).toggleWhenPressed(new RunCommand(() -> {
+			if (limelightOn == false){
+				limelightOn = true;
+				driveLimelight.execute();
+			} else {
+				limelightOn = false;
+				driveLimelight.stopLimeLight();
+			}
+		}, drivetrain));
+
+		new JoystickButton(joystick, 12).whenPressed(new RunCommand(() -> {                         // Radius, degrees
+			autoCommands.turnWithRadius(65, 170);
+		}, drivetrain));
+
+		new JoystickButton(joystick, 12).whenReleased(new RunCommand(() -> {    
+			autoCommands.stopMoving();
+		}, drivetrain));
+		
+		// Togle lights - BUTTON->
+		new JoystickButton(joystick, 13).whenPressed(new InstantCommand(() -> {
+			NetworkTable m_limelight = NetworkTableInstance.getDefault().getTable("limelight");
+			NetworkTableEntry ledMode = m_limelight.getEntry("ledMode");
+			if (ledMode.getDouble(0) == 0) {
+				ledMode.setDouble(1); // Force OFF
+			} else if (ledMode.getDouble(0) == 1) {
+				ledMode.setDouble(3); // Force ON
+			} else if (ledMode.getDouble(0) == 3) {
+				ledMode.setDouble(1); // Force OFF
+			} else {
+				ledMode.setDouble(1); // Force OFF by default
+			}
+		}));
+  }
+
+  private void configureDefaultCommands() {
+	drivetrain.setDefaultCommand(m_curvatureDrive);
+	// colorsensor.setDefaultCommand(colorSensor);
+	shooter.setDefaultCommand(runShooter);
+	// intake.setDefaultCommand(runBoth);
+	// driveLimelight.setDefaultCommand(runLimelight);
+	index.setDefaultCommand(runIndexer);
+	}
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+	  // Create a voltage constraint to ensure we don't accelerate too fast
+	  var autoVoltageConstraint =
+	  new DifferentialDriveVoltageConstraint(
+	      new SimpleMotorFeedforward(
+		  0.22,
+		  1.98,
+		  0.2),
+		  Constants.Drivetrain.mkinematics,
+	      10);
+  
+      // Create config for trajectory
+      TrajectoryConfig config =
+	  new TrajectoryConfig(
+		  AutoConstants.kMaxSpeedMetersPerSecond,
+		  AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+	      // Add kinematics to ensure max speed is actually obeyed
+	      .setKinematics(Constants.Drivetrain.mkinematics)
+	      // Apply the voltage constraint
+	      .addConstraint(autoVoltageConstraint);
+  
+      // An example trajectory to follow.  All units in meters.
+      Trajectory exampleTrajectory =
+	  TrajectoryGenerator.generateTrajectory(
+	      // Start at the origin facing the +X direction
+	      new Pose2d(0, 0, new Rotation2d(0)),
+	      // Pass through these two interior waypoints, making an 's' curve path
+	      List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
+	      // End 3 meters straight ahead of where we started, facing forward
+	      new Pose2d(3, 0, new Rotation2d(0)),
+	      // Pass config
+	      config);
+  
+      RamseteCommand ramseteCommand =
+	  new RamseteCommand(
+	      exampleTrajectory,
+	      drivetrain::getPose,
+	      new RamseteController(AutoConstants.kRamseteB, AutoConstants.kRamseteZeta),
+	      new SimpleMotorFeedforward(
+		  0.22,
+		  1.98,
+		  0.2),
+		Constants.Drivetrain.mkinematics,
+	      drivetrain::getWheelSpeeds,
+	      new PIDController(1, 0, 0),
+	      new PIDController(1, 0, 0),
+	      // RamseteCommand passes volts to the callback
+	      drivetrain::tankDriveVolts,
+	      drivetrain);
+  
+      // Reset odometry to the starting pose of the trajectory.
+      drivetrain.resetOdometry(exampleTrajectory.getInitialPose());
+  
+      // Run path following command, then stop at the end.
+      return ramseteCommand.andThen(() -> drivetrain.curvatureDrive(0.0, 0.0));
+    // An ExampleCommand will run in autonomous
+//     return new SequentialCommandGroup(new ParallelCommandGroup(new RunCommand(() -> {
+// 			shooter.shoot(-0.54);
+// 		}, shooter), new RunCommand(() -> {
+// 			index.runIndexer();
+// 		}, index)).withTimeout(10), new RunCommand(() -> {
+// 			drivetrain.curvatureDrive(-0.25, 0);
+//     }, drivetrain).withTimeout(1)).withTimeout(15);
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/ComplexDrivetrain.java
@@ -0,0 +1,327 @@
+package frc.robot.subsystems;
+
+import frc.lib.utils.PunkSparkMax;
+import frc.robot.Constants;
+// import frc.lib.utils.PunkPIDController;
+
+import com.ctre.phoenix.sensors.PigeonIMU;
+import com.revrobotics.AlternateEncoderType;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
+
+import edu.wpi.first.wpilibj.SlewRateLimiter;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+// import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.revrobotics.ControlType;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+
+
+@SuppressWarnings("PMD.ExcessiveImports")
+public class ComplexDrivetrain extends SubsystemBase {
+	private PunkSparkMax m_leftFront, m_leftBack, m_rightFront, m_rightBack;
+
+	private SpeedControllerGroup m_left, m_right;
+
+	private final DifferentialDrive m_drive;
+	private final DifferentialDriveKinematics m_kinematics;
+	private final DifferentialDriveOdometry m_odometry;
+
+	private final PIDController m_leftPIDController, m_rightPIDController;
+	private CANPIDController m_leftPID, m_rightPID;
+
+	// private DoubleSolenoid m_gearShift;
+
+	private CANEncoder m_leftEncoder, m_rightEncoder;
+
+	private PigeonIMU m_gyro;
+
+	private double[] heading;
+
+	private double velocity, highest_velocity, lowest_velocity, avgDist, lVelocity, rotations;
+
+	/**
+	 * Central class for all drivetrain-related activities.
+	 */
+	public ComplexDrivetrain() {
+		m_leftFront = new PunkSparkMax(Constants.Drivetrain.kmLeftFront, Constants.kDriveMotorConfig);
+		m_rightFront = new PunkSparkMax(Constants.Drivetrain.kmRightFront, Constants.kRightDriveConfig);
+		m_leftBack = new PunkSparkMax(Constants.Drivetrain.kmLeftBack, Constants.kDriveMotorConfig, m_leftFront);
+		m_rightBack = new PunkSparkMax(Constants.Drivetrain.kmRightBack, Constants.kRightDriveConfig, m_rightFront);
+		//m_rightFront.setInverted(true);
+		//m_rightBack.setInverted(true);
+		m_left = new SpeedControllerGroup(m_leftFront, m_leftBack);
+		m_right = new SpeedControllerGroup(m_rightFront, m_rightBack);
+		m_right.setInverted(true);
+		m_gyro = new PigeonIMU(0);
+		heading = new double[3];
+
+		m_drive = new DifferentialDrive(m_left, m_right);
+		m_kinematics = Constants.Drivetrain.mkinematics;
+		m_odometry = new DifferentialDriveOdometry(getAngle());
+
+		m_leftPIDController = new PIDController(1, 0.0015, 0);
+		m_rightPIDController = new PIDController(0.95, 0.001, 0);
+		m_leftPIDController.setTolerance(0.01, 0.005);
+		m_rightPIDController.setTolerance(0.01, 0.005);
+
+		// m_gearShift = new DoubleSolenoid(Constants.kGearShift[0], Constants.kGearShift[1]);
+		m_leftEncoder = m_leftBack.getAlternateEncoder(AlternateEncoderType.kQuadrature, 8192);
+		m_rightEncoder = m_rightBack.getAlternateEncoder(AlternateEncoderType.kQuadrature, 8192);
+		m_leftEncoder.setPosition(0);
+		m_rightEncoder.setPosition(0);
+		m_leftEncoder.setInverted(true);
+		m_leftPID = m_leftFront.getPIDController();
+		m_rightPID = m_rightFront.getPIDController();
+		m_leftPID.setFeedbackDevice(m_leftEncoder);
+		m_rightPID.setFeedbackDevice(m_rightEncoder);
+
+		// TEST Constructor - Not sure if this works.
+		// m_leftPID = new PunkPIDController(m_leftFront, Constants.Drivetrain.kPIDConfig, true);
+		// m_rightPID = new PunkPIDController(m_rightFront, Constants.Drivetrain.kPIDConfig, true);
+
+		m_leftPID.setP(0.2);
+		m_leftPID.setI(0);
+		m_leftPID.setD(0);
+		m_leftPID.setIZone(0);
+		m_leftPID.setFF(0);
+		m_leftPID.setOutputRange(-1, 1);
+		m_rightPID.setP(0.1);
+		m_rightPID.setI(1e-4);
+		m_rightPID.setD(1);
+		m_rightPID.setIZone(0);
+		m_rightPID.setFF(0);
+		m_rightPID.setOutputRange(-1, 1);
+		m_gyro.configFactoryDefault();
+
+		SmartDashboard.putData("Drivetrain - Left PID", m_leftPIDController);
+		SmartDashboard.putData("Drivetrain - Right PID", m_rightPIDController);
+		
+	}
+
+	/**
+	 * Basic drivetrain operation, with no feed-forward control.
+	 * 
+	 * @param lSpeed Left Speed of Drivetrain
+	 * @param rSpeed Right Speed of Drivetrain
+	 */
+	public void tankDrive(double lSpeed, double rSpeed) {
+		lSpeed = Constants.Drivetrain.kTankInputFactor * lSpeed;
+		rSpeed = Constants.Drivetrain.kTankInputFactor * rSpeed;
+		m_leftFront.set(lSpeed);
+		m_rightFront.set(rSpeed);
+		double speed = 0;
+		double min_speed = 0;
+		if(Math.abs(m_leftEncoder.getVelocity()) > speed) {
+			speed = m_leftEncoder.getVelocity();
+		} else if (Math.abs(m_leftEncoder.getVelocity()) < min_speed) {
+			min_speed = m_leftEncoder.getVelocity();
+		}
+		SmartDashboard.putNumber("Max Speed", speed);
+		SmartDashboard.putNumber("Min Speed", min_speed);
+		SmartDashboard.putNumber("Left_Enc", m_leftEncoder.getVelocity());
+		SmartDashboard.putNumber("Right_Enc", m_rightEncoder.getVelocity());
+		SmartDashboard.putNumber("Left_Pos", getLeftDistance());
+		SmartDashboard.putNumber("Right_Pos",  getRightDistance());
+	}
+
+	/**
+	 * Basic drivetrain operation, with no feed-forward control.
+	 * 
+	 * @param xSpeed    Speed along the x-axis (Forward is positive)
+	 * @param zRotation Rotation rate for robot (Clockwise is positive)
+	 */
+	SlewRateLimiter filter = new SlewRateLimiter(0.9);
+	public void curvatureDrive(double xSpeed, double zRotation) {
+		m_drive.curvatureDrive(filter.calculate(xSpeed), zRotation * -1, true);
+	}
+
+	/**
+	 * PID-Controlled drivetrain, with kinematics and odometry to be more precise.
+	 * 
+	 * @param _speeds Individual speeds of each side.
+	 */
+	public void pidTest(DifferentialDriveWheelSpeeds _speeds) {
+		final double leftOutput = m_leftPIDController.calculate(m_leftEncoder.getVelocity() / 610, _speeds.leftMetersPerSecond);
+		final double rightOutput = m_rightPIDController.calculate(m_rightEncoder.getVelocity() / 610,
+				_speeds.rightMetersPerSecond);
+		m_leftFront.setVoltage(leftOutput);
+		m_rightFront.setVoltage(rightOutput);
+	}
+
+	/**
+	 * PID-Controlled drivetrain, but without kinematics or odometry.
+	 * 
+	 * @param lSpeed Left Speed of Drivetrain
+	 * @param rSpeed Right Speed of Drivetrain
+	 */
+	public void pidTankDrive(double lSpeed, double rSpeed) {
+		updateOdometry();
+		final double leftOutput = m_leftPIDController.calculate(m_leftEncoder.getVelocity() / 632, lSpeed);
+		final double rightOutput = m_rightPIDController.calculate(m_rightEncoder.getVelocity() / 632, rSpeed);
+		m_leftFront.set(leftOutput * 0.5); // Shifted from 1.0 to 0.5
+		m_rightFront.set(rightOutput * 0.5); // Same as line previous line
+		SmartDashboard.putNumber("Left_Enc", m_leftEncoder.getVelocity());
+		SmartDashboard.putNumber("Right_Enc", m_rightEncoder.getVelocity());
+		// SmartDashboard.putNumber("Left_Pos", m_leftEncoder.getPosition());
+		// SmartDashboard.putNumber("Right_Pos", m_rightEncoder.getPosition());
+		SmartDashboard.putNumber("left_out", leftOutput);
+		SmartDashboard.putNumber("right_out", rightOutput);
+		SmartDashboard.putNumber("Drivetrain - Left Error", m_leftPIDController.getVelocityError());
+		SmartDashboard.putNumber("Drivetrain - Left Setpoint", lSpeed);
+
+	}
+
+	/**
+	 * EXPERIMENTAL: Go to a set distance (in feet)
+	 * 
+	 * @param distance     How far you should go (in feet)
+	 */
+	public void setPosition(double distance) {
+		distance = (distance / 12) / (Math.PI * 6); // Converts to Revolutions
+		m_leftPID.setReference(distance, ControlType.kPosition);
+		m_rightPID.setReference(distance, ControlType.kPosition);
+	}
+
+	/**
+	 * EXPERIMENTAL: Go to a set speed (in feet per second)
+	 * 
+	 * @param velocity     How fast you should go (in feet/s)
+	 */
+	public void setVelocity(double lSpeed, double rSpeed) {
+		lVelocity = (lSpeed * 12) / (Math.PI * 6) / 60;
+		velocity = (velocity * 12) / (Math.PI * 6) / 60;
+		lSpeed = lSpeed * 610;
+		SmartDashboard.putNumber("Left_Enc", m_leftEncoder.getVelocity());
+		SmartDashboard.putNumber("Right_Enc", m_rightEncoder.getVelocity());
+		SmartDashboard.putNumber("Velocity", lVelocity);
+		SmartDashboard.putNumber("FPS Velocity", ((lVelocity / 60) * (6 * Math.PI)));
+		SmartDashboard.putNumber("Drivetrain - Left Setpoint", lSpeed);
+		m_leftPID.setReference(lSpeed, ControlType.kVelocity);
+		// m_rightPID.setReference(rSpeed, ControlType.kVelocity);
+	}
+	/**
+	 * EXPERIMENTAL: Go to a set speed (in feet per second)
+	 * 
+	 * @param velocity     How fast you should go (in feet/s)
+	 */
+	public void testVelocity(double lSpeed, double rSpeed) {
+		double rot = lSpeed * 5700;
+		double rot2 = rSpeed * 5700;
+		
+		SmartDashboard.putNumber("Left Rotations", rot);
+		SmartDashboard.putNumber("Right Rotations", rot2);
+		SmartDashboard.putNumber("Drivetrain - Left Setpoint", lSpeed);
+		m_leftPID.setReference(rot, ControlType.kVelocity);
+		m_rightPID.setReference(rot2, ControlType.kVelocity);
+		SmartDashboard.putNumber("Left_Enc", m_leftEncoder.getVelocity());
+		SmartDashboard.putNumber("Right_Enc", m_rightEncoder.getVelocity());
+	}
+
+	/**
+	 * Set right motor group speed.
+	 * @param speed Speed to set motor group to.
+	 */
+	public void setRight(double speed) {
+		m_right.set(speed);
+	}
+
+	/**
+	 * Set left motor group speed.
+	 * @param speed Speed to set motor group to.
+	 */
+	public void setLeft(double speed) {
+		m_left.set(speed);
+	}
+
+	/**
+	 * PID-Controlled curvatureDrive equivalent.
+	 * 
+	 * @param xSpeed    Speed along the x-axis (Forward is positive)
+	 * @param zRotation Rotation rate for robot (Clockwise is positive)
+	 */
+	public void drive(double xSpeed, double rot) {
+		updateOdometry();
+		var wheelSpeeds = m_kinematics.toWheelSpeeds(new ChassisSpeeds(xSpeed, 0.0, rot));
+		pidTest(wheelSpeeds);
+	}
+
+	/**
+	 * Shifts the pancake motors on each of the gearboxes.
+	 * 
+	 * @param value State to reach (Can be forward, reverse, or off.)
+	 */
+	public void shiftGears(Value value) {
+		// m_gearShift.set(value);
+	}
+
+	/**
+	 * Retrieves the z-axis rotation from the PigeonIMU.
+	 * 
+	 * @return Current rotation around z-axis (yaw).
+	 */
+
+	public Rotation2d getAngle() {
+		m_gyro.getYawPitchRoll(heading);
+		return Rotation2d.fromDegrees(heading[0]);
+	}
+
+	/**
+	 * Updates the odometry object with the latest sensor data.
+	 */
+	private void updateOdometry() {
+		m_odometry.update(getAngle(), m_leftEncoder.getPosition(), m_rightEncoder.getPosition());
+	}
+
+	/**
+	 * Retrieves the displacement (in feet) on the left side of the drivetrain.
+	 * 
+	 * @return Left-side displacement
+	 */
+	public double getLeftDistance() {
+		return (m_leftEncoder.getPosition() * (Math.PI * 6)) / 12;
+	}
+
+	/**
+	 * Retrieves the displacement (in feet) on the right side of the drivetrain.
+	 * 
+	 * @return Right-side displacement
+	 */
+	public double getRightDistance() {
+		return (m_rightEncoder.getPosition() * (Math.PI * 6)) / 12;
+	}
+
+	public void resetOdometry(Pose2d pose) {
+		reset();
+		m_odometry.resetPosition(pose, m_gyro.);
+	      }
+
+	/**
+	 * Retrieves the average displacement (in meters)between each side of the
+	 * drivetrain.
+	 * 
+	 * @return Average displacement between each encoder.
+	 */
+	public double getDistance() {
+		avgDist = ((getLeftDistance() + getRightDistance()) / 2);
+		SmartDashboard.putNumber("avgDist", avgDist);
+		return avgDist;
+	}
+
+	/**
+	 * Resets the encoders to zero, for autonomous commands and diagnostics.
+	 */
+	public void reset() {
+		m_leftEncoder.setPosition(0);
+		m_rightEncoder.setPosition(0);
+	}
+}
